506d169f8084219ab50742e7a0c8865a5756829a
talhakhanp <talha.khan@stud.hshl.de>
2024-11-22 01:51:22 +0100

    Update TrafficLight.c

--- Lab1/TrafficLight.c
+++ Lab1/TrafficLight.c
@@ -1,5 +1,7 @@
-// C++ code
-//
+/// code for the tinkercad simulation
+
+// BY TALHA KHAN ONLY FOR EDUCATIONAL PURPOSES
+
 void setup()
 {
   pinMode(2, OUTPUT);
@@ -9,20 +11,20 @@ void setup()
 
 void loop()
 {
-  digitalWrite(2, HIGH);
-  delay(5000); // Wait for 1000 millisecond(s)
+  digitalWrite(3, LOW);  
+  digitalWrite(2, HIGH);  // red only
+  delay(5000);
   
-  digitalWrite(3, HIGH);
-  delay(500); // Wait for 500 millisecond(s)
+  digitalWrite(3, HIGH); // // red + yellow
+  delay(2000);
+  
   digitalWrite(2, LOW);
-  delay(5000); // Wait for 1000 millisecond(s)
+  digitalWrite(3, LOW);
+   digitalWrite(4, HIGH); // green
+  delay(5000); 
   
-   digitalWrite(4, HIGH);
-  delay(500); // Wait for 500 millisecond(s)
-    digitalWrite(3, LOW);
-  delay(5000); // Wait for 1000 millisecond(s)
-  
- 
   digitalWrite(4, LOW);
-  delay(5000); // Wait for 1000 millisecond(s)
+  digitalWrite(3, HIGH); // // yellow
+  delay(2000);
+  
 }
